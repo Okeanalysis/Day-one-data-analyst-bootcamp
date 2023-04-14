bece110077f1da5154544d58af3e08d3b7a3e0bc
emmanuel214 <77278849+emmanuel214@users.noreply.github.com>
2023-04-14 19:55:55 +0100

    Add files via upload

---
+++ creation code.sql
@@ -0,0 +1,12 @@
+CREATE TABLE EmployeeDemographics
+(EmployeeID int, 
+FirstName Varchar(50),
+LastName Varchar(50),
+Age int,
+Gender Varchar(50))
+
+
+CREATE TABLE EmployeeSalary
+(EmployeeID int,
+JOBTITLE Varchar(50),
+Salary int)
